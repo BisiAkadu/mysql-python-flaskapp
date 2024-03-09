f67e6b1f420998ed3e7defbcec5463d353b3a317
temiladebello <temiladebello@gmail.com>
2024-03-09 20:19:42 +0100

    correction on sql connector microserv

--- mysql/database-employee.sql
+++ mysql/database-employee.sql
@@ -1,4 +1,4 @@
-USE testdb;
+USE bisidb;
 
 CREATE TABLE staff(
     EmployeeID int not null AUTO_INCREMENT,
@@ -8,11 +8,11 @@ CREATE TABLE staff(
     PRIMARY KEY (EmployeeID)
 );
 
-INSERT INTO employee(FirstName, Surname, Email)
+INSERT INTO staff(FirstName, Surname, Email)
 VALUES("Ayeni", "clement","ayeni@yahoo.com"), ("Philips", "Ojo","ojokomoalfe@yahoo.com"),("babalola", "joshua","joshua@yahoo.com"); 
-INSERT INTO employee(FirstName, Surname, Email)
+INSERT INTO staff(FirstName, Surname, Email)
 VALUES("bisi", "Folape","bisi@yahoo.com"), ("kofo", "Ojooba","kofo@yahoo.com"),("john", "john","john@yahoo.com");
-INSERT INTO employee(FirstName, Surname, Email)
+INSERT INTO staff(FirstName, Surname, Email)
 VALUES("Ayen", "clemen","ayen@yahoo.com"), ("Philip", "Ojos","ojokomolfe@yahoo.com"),("babalol", "joshu","joshu@yahoo.com"); 
-INSERT INTO employee(FirstName, Surname, Email)
+INSERT INTO staff(FirstName, Surname, Email)
 VALUES("bisi", "Folap","bis@yahoo.com"), ("kfo", "Ojoba","kofo@yaho.com"),("joh", "joh","joh@yahoo.com");
--- python/app.py
+++ python/app.py
@@ -1,12 +1,10 @@
 import mysql.connector
 
 connection = mysql.connector.connect(
-    user='root', password='Oluwatomisin1$', host='mysql', port="3306", database='bisidb')
+    user='root', password='*********', host='mysql', port="3306", database='bisidb')
 print("DataBase connect")
-
 cursor = connection.cursor()
 cursor.execute('Select * FROM staff')
 staff = cursor.fetchall()
 connection.close()
-
 print(staff)
