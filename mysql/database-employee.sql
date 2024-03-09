e2afa11b4dd3fb41ceac4eb9de48f8be577abd0f
temiladebello <temiladebello@gmail.com>
2024-03-09 19:22:23 +0100

    add new file

---
+++ mysql/database-employee.sql
@@ -0,0 +1,18 @@
+USE testdb;
+
+CREATE TABLE staff(
+    EmployeeID int not null AUTO_INCREMENT,
+    FirstName varchar(100) NOT NULL,
+    Surname varchar(100) NOT NULL,
+    Email varchar(100) NOT NULL,
+    PRIMARY KEY (EmployeeID)
+);
+
+INSERT INTO employee(FirstName, Surname, Email)
+VALUES("Ayeni", "clement","ayeni@yahoo.com"), ("Philips", "Ojo","ojokomoalfe@yahoo.com"),("babalola", "joshua","joshua@yahoo.com"); 
+INSERT INTO employee(FirstName, Surname, Email)
+VALUES("bisi", "Folape","bisi@yahoo.com"), ("kofo", "Ojooba","kofo@yahoo.com"),("john", "john","john@yahoo.com");
+INSERT INTO employee(FirstName, Surname, Email)
+VALUES("Ayen", "clemen","ayen@yahoo.com"), ("Philip", "Ojos","ojokomolfe@yahoo.com"),("babalol", "joshu","joshu@yahoo.com"); 
+INSERT INTO employee(FirstName, Surname, Email)
+VALUES("bisi", "Folap","bis@yahoo.com"), ("kfo", "Ojoba","kofo@yaho.com"),("joh", "joh","joh@yahoo.com");
---
+++ python/app.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    user='root', password='Oluwatomisin1$', host='mysql', port="3306", database='bisidb')
+print("DataBase connect")
+
+cursor = connection.cursor()
+cursor.execute('Select * FROM staff')
+staff = cursor.fetchall()
+connection.close()
+
+print(staff)
